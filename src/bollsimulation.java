4103c84a73e5282b4da25337317688a98a49d530
sumsar8 <rasmus.oberg@elev.ga.ntig.se>
2021-08-26 13:34:25 +0200

    Start works

--- src/bollsimulation.java
+++ src/bollsimulation.java
@@ -1,5 +1,14 @@
-class bollsimulation {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class bollsimulation {
+    public static void main(String[] args) {
+        int ballweight = 1;
+        int startheight = 10;
+
+        Scanner scanner = new Scanner(System.in);
+        String starttext = scanner.nextLine();
+        if(starttext .equals("start")){
+
+        }
     }
 }
