e44009c22d4dc9b64eb5840d466ad0e768b21735
sumsar8 <rasmus.oberg@elev.ga.ntig.se>
2021-08-28 10:32:22 +0200

    ||

--- src/bollsimulation.java
+++ src/bollsimulation.java
@@ -1,14 +1,23 @@
 import java.util.Scanner;
 
 public class bollsimulation {
+    private static double bally = 10;
+    private static double ballweight = 1;
+    private static double gravitation = 9.82;
+    private static double time;
+
     public static void main(String[] args) {
-        int ballweight = 1;
-        int startheight = 10;
+
 
         Scanner scanner = new Scanner(System.in);
         String starttext = scanner.nextLine();
-        if(starttext .equals("start")){
+        if(starttext .equals("start")||starttext .equals("Start")){
 
+                runsimulation();
+                System.out.println(bally);
         }
+    }
+    public static void runsimulation(){
+        bally = bally - 1;
     }
 }
